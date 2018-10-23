1e01374bec9f051560676bf86667977dd67e36b6
Mohammed Shaikh <afmdnf@berkeley.edu>
2018-10-23 02:18:25 -0700

    Added UnionFind

---
+++ UnionFind.java
@@ -0,0 +1,34 @@
+public class UnionFind {
+
+	private int[] parent, size;
+
+	public UnionFind(int N) {
+		parent, size = new int[N];
+		for (int i = 0; i < N; ++i) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+	}
+
+	private int find(int x) {
+		// uses PATH COMPRESSION
+		if (x != parent[x]) parent[x] = find(parent[x]);
+		return parent[x];
+	}
+
+	public boolean isConnected(int x, int y) {
+		return find(x) == find(y);
+	}
+
+	public void union(int x, int y) {
+		int xr = find(x), yr = find(y);
+		if (xr == yr) return;
+		// uses WEIGHTED UNION
+		if (size[xr] < size[yr]) { parent[xr] = yr; size[yr] += size[xr]; }
+		else { parent[yr] = xr; size[xr] += size[yr]; }
+	}
+
+	public int size(int x) {
+		return size[find(x)];
+	}
+}
